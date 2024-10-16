8cedbdf673381a07678ab97830c8f565b5898a73
Jayavel <jayavel1520@gmail.com>
2024-10-16 21:03:10 +0530

    Added Popup for getting user input

---
+++ app.js
@@ -0,0 +1,8 @@
+// const addNote = document.getElementById("addNote");
+// const noteValue = document.getElementById("noteContent");
+// console.log(noteValue);
+
+// addNote.addEventListener("click",()=>{
+    
+// })
+
